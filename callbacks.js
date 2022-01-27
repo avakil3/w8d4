125dff739a6d7c249acfec67141e95a6e132a322
avakil3 <aagamvakil@hotmail.com>
2022-01-27 14:57:57 -0500

    finished lamps

--- callbacks.js
+++ callbacks.js
@@ -28,6 +28,58 @@ class Clock {
 	}
 }
   
+//   let clocky = new Clock();
+// console.log(clocky.printTime());
 
-  let clocky = new Clock();
-console.log(clocky.printTime());
+// const readline = require('readline');
+// const reader = readline.createInterface({
+// 	input: process.stdin,
+// 	output: process.stdout
+// });
+
+function addNumbers(sum, numsLeft, completionCallback){
+	if(numsLeft > 0){
+		reader.question("Input a number:", function (answer) {   
+			let integer = parseInt(answer);
+			sum += integer;
+			numsLeft--;
+			console.log(`Sum is ${sum}`);
+			addNumbers(sum,numsLeft,completionCallback);
+			
+		 });
+	} else{
+		reader.close();
+		return completionCallback(sum);
+	}
+	
+}
+// console.log(addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`)));
+
+Function.prototype.myBind = function(context){
+	//this is "turnOn"
+	//this.call(context)
+	return () => {this.call(context)}
+};
+
+// Function.prototype.myBind = (context) => {this.call(context)};
+
+
+class Lamp {
+	constructor() {
+	  this.name = "a lamp";
+	}
+  }
+  
+  const turnOn = function() {
+	console.log("Turning on " + this.name);
+  };
+  
+  const lamp = new Lamp();
+  
+  turnOn(); // should not work the way we want it to
+  
+  const boundTurnOn = turnOn.bind(lamp);
+  const myBoundTurnOn = turnOn.myBind(lamp);
+  
+  boundTurnOn(); // should say "Turning on a lamp"
+  myBoundTurnOn(); // should say "Turning on a lamp"
